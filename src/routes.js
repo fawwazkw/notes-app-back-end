83efa8ac629a993a9410a052aec39bd69512a854
Fawwaz <fawwazdani@student.ub.ac.id>
2023-02-03 17:09:39 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+    options: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+];
+ 
+module.exports = routes;
